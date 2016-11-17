3d5762ca6d93e09c62b0e943f0f847ac646adc88
Mark C Mitchell <mark@markcmitchell.net>
2016-11-17 14:52:04 +1100

    Move ca-majority code to ca-majority module; CaMajority is now a class

---
+++ src/ca-majority.js
@@ -0,0 +1,95 @@
+import utils from './utils';
+import Color from 'color-js';
+
+export default class CaMajority {
+
+  constructor(width=8, height=8, numOfStates=12, r=2, cellSize=1) {
+    this.width = width;
+    this.height = height;
+    this.numOfStates = numOfStates;
+    this.r = r; // todo:: what is r?
+    this.cellSize = cellSize;
+    this.state = 0;
+
+    this.initConfig();
+    this.canvas = document.createElement('canvas');
+    this.ctx = this.canvas.getContext('2d');
+    this.ctx.canvas.width = window.innerWidth;
+    this.ctx.canvas.height = window.innerHeight;
+    document.body.appendChild(this.canvas);
+  }
+
+  initConfig() {
+    this.config = utils.createMatrix(this.height, this.width, 0);
+    this.nextConfig = utils.createMatrix(this.height, this.width, 0);
+    this.time = 0;
+
+    for(let y=0; y<this.height; y++) {
+      for(let x=0; x<this.width; x++) {
+        this.state = utils.getRandomIntInclusive(0, this.numOfStates-1);
+        this.config[y][x] = this.state;
+      }
+    }
+  }
+
+  step() {
+    this.time += 1;
+    console.log('time: ', this.time);
+
+    for(let x=0; x<this.width; x++) {
+      for(let y=0; y<this.height; y++) {
+        this.state = this.config[y][x];
+        let counts = new Array(this.numOfStates).fill(0);
+        for(let dx=-this.r; dx<this.r+1; dx++) {
+          for(let dy=-this.r; dy<this.r+1; dy++) {
+            let col = (y+dy) % this.height;
+            let row = (x+dx) % this.width;
+            if(Math.sign(col) === -1) {
+              col = this.height + col;
+            }
+            if(Math.sign(row) === -1) {
+              row = this.width + col;
+            }
+            let s = this.config[col][row];
+            counts[s] += 1;
+          }
+        }
+        let maxCount = Math.max(...counts);
+        let maxStates = [];
+
+        for(let i=0; i<this.numOfStates; i++) {
+          if(counts[i] === maxCount) {
+            maxStates.push(i);
+          }
+        }
+        this.state = maxStates[Math.floor(Math.random() * maxStates.length)];
+        this.nextConfig[y][x] = this.state;
+      }
+    }
+
+    // how necessary is the following?
+    let temp = this.config;
+    this.config = this.nextConfig;
+    this.nextConfig = temp;
+
+    this.render();
+
+    if(this.time < 15) {
+      setTimeout(() => {
+        window.requestAnimationFrame(this.step.bind(this));
+      }, 100);
+    } else {
+      console.log('%cSTOPPED', 'font-size: 24px; text-decoration: underline; color: blue');
+    }
+  }
+
+  render() {
+    for(let y=0; y<this.height; y++) {
+      for(let x=0; x<this.width; x++) {
+        let fillStyle = Color('#ff0000').shiftHue(this.config[y][x] * Math.floor(360/this.numOfStates));
+        this.ctx.fillStyle = fillStyle;
+        this.ctx.fillRect(x*this.cellSize, y*this.cellSize, this.cellSize, this.cellSize);
+      }
+    }
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 'use strict';
 
 import dat from 'dat-gui';
-import utils from './utils';
 
-const Color = require('color-js');
+import CaMajority from './ca-majority';
 
+let x = new CaMajority(64,64);
+x.step();
 
 const gui = new dat.gui.GUI();
 var obj = {
@@ -33,101 +34,3 @@ colorFolder.addColor(obj, 'color0');
 colorFolder.addColor(obj, 'color1');
 colorFolder.addColor(obj, 'color2');
 colorFolder.addColor(obj, 'color3');
-
-
-var canvas = document.createElement('canvas');
-var ctx = canvas.getContext('2d');
-ctx.canvas.width  = window.innerWidth;
-ctx.canvas.height = window.innerHeight;
-// ctx.fillStyle = '#0066cc';
-// ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-
-var width = 8*2;
-var height = 8*2;
-var numOfStates = 12;
-var r = 2;
-
-var time;
-var config = utils.createMatrix(height, width, 0);
-var nextConfig = utils.createMatrix(height, width, 0);
-
-var state;
-
-init();
-step();
-
-function init() {
-  time = 0;
-
-  for(let y=0; y<height; y++) {
-    for(let x=0; x<width; x++) {
-      state = utils.getRandomIntInclusive(0, numOfStates-1);
-      config[y][x] = state;
-    }
-  }
-}
-
-function draw() {
-  let step = 2;
-  let cellSize = 2;
-  for(let y=0; y<height; y++) {
-    for(let x=0; x<width; x++) {
-      let fillStyle = Color('#ff0000').shiftHue(config[y][x] * Math.floor(360/numOfStates));
-      ctx.fillStyle = fillStyle;
-      ctx.fillRect(x*step, y*step, cellSize, cellSize);
-    }
-  }
-}
-
-function step() {
-  time++;
-  // console.log('time: ', time);
-
-  for(let x=0; x<width; x++) {
-    for(let y=0; y<height; y++) {
-      state = config[y][x];
-      let counts = new Array(numOfStates).fill(0);
-      for(let dx=-r; dx<r+1; dx++) {
-        for(let dy=-r; dy<r+1; dy++) {
-          let col = (y+dy) % height;
-          let row = (x+dx) % width;
-          if(Math.sign(col) === -1) {
-            col = height+col;
-          }
-          if(Math.sign(row) === -1) {
-            row = width+col;
-          }
-          let s = config[col][row];
-          counts[s] += 1;
-        }
-      }
-      var maxCount = Math.max(...counts);
-
-      var maxStates = [];
-
-      for(var i=0; i<numOfStates; i++) {
-        if(counts[i] === maxCount) {
-          maxStates.push(i);
-        }
-      }
-      // console.log(maxStates);
-      state = maxStates[Math.floor(Math.random()*maxStates.length)];
-
-      nextConfig[y][x] = state;
-    }
-  }
-  var temp = config;
-  config = nextConfig;
-  nextConfig = temp;
-  draw();
-  if(time < 15) {
-    setTimeout(() => {
-      window.requestAnimationFrame(step);
-    }, 100);
-  } else {
-    console.log('%cSTOPPED', 'font-size: 24px; text-decoration: underline; color: blue');
-  }
-
-}
-
-document.body.appendChild(canvas);
