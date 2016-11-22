3070dacbd4da20efc5969812ad270869f233377e
Mark C Mitchell <mark@markcmitchell.net>
2016-11-22 16:38:47 +1100

    Refactor GUI code out of index.js into gui.js module

---
+++ src/gui.js
@@ -0,0 +1,74 @@
+import dat from 'dat-gui';
+import Color from 'color-js';
+import CaMajority from './ca-majority';
+
+export default class CaMajorityGUI {
+
+  constructor({
+    width = 8,
+    height = 8,
+    numOfStates = 12,
+    cellSize = 1,
+    r = 2,
+    colors = undefined
+  } = {}) {
+    this.width = width;
+    this.height = height;
+    this.numOfStates = numOfStates;
+    this.cellSize = cellSize;
+    this.r = r;
+    this.restart = function() {
+      if(this.caMajority) {
+        this.destroy();
+      }
+      this.caMajority = new CaMajority({
+        width,
+        height,
+        numOfStates,
+        cellSize,
+        r,
+        colors: Object.values(this.colors) // convert to array
+      });
+      this.caMajority.run();
+    };
+    this.destroy = function() {
+      this.caMajority.destroy();
+    };
+
+    this.automaticColors = false;
+
+    this.gui = new dat.gui.GUI();
+    this.gui.remember(this);
+    this.gui.add(this, 'width').min(0);
+    this.gui.add(this, 'height').min(0);
+    this.gui.add(this, 'numOfStates').min(2).onFinishChange(this.reloadColorSelection.bind(this));
+    this.gui.add(this, 'cellSize').min(1);
+    this.gui.add(this, 'r').min(1).step(1);
+    this.gui.add(this, 'restart');
+    this.gui.add(this, 'destroy');
+    this.gui.add(this, 'reloadColorSelection');
+
+    this.colorFolder = this.gui.addFolder('colors');
+    this.colorFolder.open();
+
+    this.colorControllers = [];
+    this.colors = {};
+    this.reloadColorSelection();
+    this.restart();
+  }
+
+  reloadColorSelection() {
+    this.colorControllers.forEach(color => {
+      this.colorFolder.remove(color);
+    });
+    this.colorControllers = [];
+    for(let i=0; i<this.numOfStates; i++) {
+      this.colors[`color${i}`] = getEquallySpacedColor.call(this, i);
+      this.colorControllers.push(this.colorFolder.addColor(this.colors, `color${i}`));
+    }
+
+    function getEquallySpacedColor(i=0) {
+      return Color('#ff0000').shiftHue(i * Math.floor(360/this.numOfStates)).toCSS();
+    }
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,73 +1,9 @@
-import dat from 'dat-gui';
-import Color from 'color-js';
-import CaMajority from './ca-majority';
+import CaMajorityGUI from './gui';
 
-const gui = new dat.gui.GUI();
-
-var opts = {
-    width: 256,
-    height: 128,
-    numOfStates: 12,
-    cellSize: 2,
-    r: 2,
-    restart: function() {
-      this.destroy();
-      x = new CaMajority({
-        width: this.width,
-        height: this.height,
-        numOfStates: this.numOfStates,
-        cellSize: this.cellSize,
-        r: this.r,
-        automaticColors: this.automaticColors,
-        colors: Object.values(colors) // convert to array
-      });
-      x.run();
-    },
-    destroy: function() {
-      x.destroy();
-    },
-    automaticColors: false,
-    reloadColorSelection
-};
-
-gui.remember(obj);
-gui.add(opts, 'width').min(0);
-gui.add(opts, 'height').min(0);
-gui.add(opts, 'numOfStates').min(2).onFinishChange(reloadColorSelection);
-gui.add(opts, 'cellSize').min(1);
-gui.add(opts, 'r').min(1).step(1);
-gui.add(opts, 'restart');
-gui.add(opts, 'destroy');
-gui.add(opts, 'reloadColorSelection');
-let colorFolder = gui.addFolder('colors');
-colorFolder.open();
-
-let colorControllers = [];
-let colors = {};
-
-reloadColorSelection();
-
-let x = new CaMajority({
+let gui = new CaMajorityGUI({
   width: 256,
   height: 128,
   numOfStates: 12,
   cellSize: 2,
-  colors: Object.values(colors) // convert to array
+  r: 2
 });
-x.run();
-
-
-function reloadColorSelection() {
-  colorControllers.forEach(color => {
-    colorFolder.remove(color);
-  });
-  colorControllers = [];
-  for(let i=0; i<obj.numOfStates; i++) {
-    colors[`color${i}`] = getEquallySpacedColor(i);
-    colorControllers.push(colorFolder.addColor(colors, `color${i}`));
-  }
-
-  function getEquallySpacedColor(i=0) {
-    return Color('#ff0000').shiftHue(i * Math.floor(360/obj.numOfStates)).toCSS();
-  }
-}
