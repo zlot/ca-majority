bea41bff78895f62e887b3b9871b4e15be3d0c98
Mark C Mitchell <mark@markcmitchell.net>
2016-11-29 16:48:09 +1100

    Missing semicolon

--- src/ca-majority.js
+++ src/ca-majority.js
@@ -47,7 +47,7 @@ export default class CaMajority {
 
     this.canvas = document.createElement('canvas');
     this.ctx = this.canvas.getContext('2d');
-    this.ctx.canvas.width = this.width
+    this.ctx.canvas.width = this.width;
     this.ctx.canvas.height = this.height;
     document.body.appendChild(this.canvas);
   }
